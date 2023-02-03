bdccaa68f46ef9f32cce1e665667665cd93a6acf
edward <whuqifei@163.com>
2023-02-03 22:36:47 +0800

    update prediction_step

--- trainer_hf.py
+++ trainer_hf.py
@@ -2595,14 +2595,20 @@ class Trainer:
             Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
             logits and labels (each being optional).
         """
-        has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
         # For CLIP-like models capable of returning loss values.
         # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
         # is `True` in `model.forward`.
         return_loss = inputs.get("return_loss", None)
         if return_loss is None:
             return_loss = self.can_return_loss
-        loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
+
+        loss_without_labels = False
+        if len(self.label_names) == 0:
+            has_labels = False
+            if return_loss:
+                loss_without_labels = True
+        else:
+            has_labels = all(inputs.get(k) is not None for k in self.label_names)
 
         inputs = self._prepare_inputs(inputs)
         if ignore_keys is None:
@@ -2612,7 +2618,7 @@ class Trainer:
                 ignore_keys = []
 
         # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
-        if has_labels or loss_without_labels:
+        if has_labels:
             labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
             if len(labels) == 1:
                 labels = labels[0]
