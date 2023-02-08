131d61f9052690d50d15b9c94499ad1456baf553
edward <whuqifei@163.com>
2023-02-08 22:34:15 +0800

    update eval

--- trainer_hf.py
+++ trainer_hf.py
@@ -2133,6 +2133,46 @@ class Trainer:
         self._memory_tracker.stop_and_update_metrics(output.metrics)
 
         return PredictionOutput(predictions=output.predictions, label_ids=output.label_ids, metrics=output.metrics)
+    
+    def _numpify_concat_truncate(self, input_host, output_all, num_samples=-1, is_nested=True):
+        """nested_numpify data and concat it to output"""
+        if input_host is None:
+            return output_all
+
+        input_host_numpify = nested_numpify(input_host)
+        if output_all is None:
+            output_all = input_host_numpify
+        else:
+            if is_nested:
+                output_all = nested_concat(output_all, input_host_numpify, padding_index=-100)
+            else:
+                output_all = np.concatenate((output_all, input_host_numpify), axis=0)
+
+        # Number of losses has been rounded to a multiple of batch_size and in a distributed training, the number of
+        # samplers has been rounded to a multiple of batch_size, so we truncate.
+        if num_samples != -1 and output_all is not None:
+            if is_nested:
+                output_all = nested_truncate(output_all, num_samples)
+            else:
+                output_all = output_all[:num_samples]
+        return output_all
+    
+    def _pad_gather_concat(self, input_data, output, labels=None, batch_size=None, is_loss=False, is_logits=False):
+        """pad data and nested_concat"""
+        if input_data is None:
+            return output
+        
+        if is_loss:
+            input_data = self._nested_gather(input_data.repeat(batch_size))
+            output = input_data if output is None else torch.cat((output, input_data), dim=0)
+        else:
+            inputs_data = self._pad_across_processes(inputs_data)
+            inputs_data = self._nested_gather(inputs_data)
+            if is_logits and self.preprocess_logits_for_metrics is not None:
+                inputs_data = self.preprocess_logits_for_metrics(inputs_data, labels)
+            output = inputs_data if output is None else nested_concat(output, inputs_data, padding_index=-100)
+
+        return output
 
     def evaluation_loop(
         self,
@@ -2143,23 +2183,18 @@ class Trainer:
         metric_key_prefix: str = "eval",
     ) -> EvalLoopOutput:
         """
-        Prediction/evaluation loop, shared by `Trainer.evaluate()` and `Trainer.predict()`.
-
-        Works both with or without labels.
+        Predict/eval loop, shared by Trainer.evaluate() and Trainer.predict(). Works both with or without labels.
         """
         args = self.args
         prediction_loss_only = prediction_loss_only if prediction_loss_only is not None else args.prediction_loss_only
 
         # if eval is called w/o train init deepspeed here
         if args.deepspeed and not self.deepspeed:
-            # XXX: eval doesn't have `resume_from_checkpoint` arg but we should be able to do eval
-            # from the checkpoint eventually
-            deepspeed_engine, _, _ = deepspeed_init(
-                self, num_training_steps=0, resume_from_checkpoint=None, inference=True
-            )
-            self.model = deepspeed_engine.module
-            self.model_wrapped = deepspeed_engine
-            self.deepspeed = deepspeed_engine
+            # eval doesn't have resume_from_checkpoint arg but we should be able to do eval from the checkpoint
+            ds_engine, _, _ = deepspeed_init(self, num_training_steps=0, resume_from_checkpoint=None, inference=True)
+            self.model = ds_engine.module
+            self.model_wrapped = ds_engine
+            self.deepspeed = ds_engine
 
         model = self._wrap_model(self.model, training=False, dataloader=dataloader)
 
@@ -2172,13 +2207,9 @@ class Trainer:
                 model = model.to(dtype=torch.bfloat16, device=args.device)
 
         batch_size = self.args.eval_batch_size
-
-        logger.info(f"***** Running {description} *****")
-        logger.info(f"  Num examples = {self.num_examples(dataloader)}")
-        logger.info(f"  Batch size = {batch_size}")
+        logger.info(f"Running {description}, num examples = {self.num_examples(dataloader)}, batch size = {batch_size}")
 
         model.eval()
-
         self.callback_handler.eval_dataloader = dataloader
         # Do this before wrapping.
         eval_dataset = getattr(dataloader, "dataset", None)
@@ -2186,122 +2217,50 @@ class Trainer:
         if args.past_index >= 0:
             self._past = None
 
-        # Initialize containers
-        # losses/preds/labels on GPU/TPU (accumulated for eval_accumulation_steps)
-        losses_host = None
-        preds_host = None
-        labels_host = None
-        inputs_host = None
+        # losses/preds/labels on GPU (accumulated for eval_accumulation_steps), all prefix is on CPU (final containers)
+        losses_host, preds_host, labels_host, inputs_host = None, None, None, None
+        all_losses, all_preds, all_labels, all_inputs = None, None, None, None
 
-        # losses/preds/labels on CPU (final containers)
-        all_losses = None
-        all_preds = None
-        all_labels = None
-        all_inputs = None
-        # Will be useful when we have an iterable dataset so don't know its length.
-
-        observed_num_examples = 0
         # Main evaluation loop
         for step, inputs in enumerate(dataloader):
-            # Update the observed num examples
-            observed_batch_size = find_batch_size(inputs)
-            if observed_batch_size is not None:
-                observed_num_examples += observed_batch_size
-                # For batch samplers, batch_size is not known by the dataloader in advance.
-                if batch_size is None:
-                    batch_size = observed_batch_size
+            # For batch samplers, batch_size is not known by the dataloader in advance.
+            if batch_size is None:
+                batch_size = find_batch_size(inputs)
 
             # Prediction step
             loss, logits, labels = self.prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
             inputs_decode = self._prepare_input(inputs["input_ids"]) if args.include_inputs_for_metrics else None
 
             # Update containers on host
-            if loss is not None:
-                losses = self._nested_gather(loss.repeat(batch_size))
-                losses_host = losses if losses_host is None else torch.cat((losses_host, losses), dim=0)
-            if labels is not None:
-                labels = self._pad_across_processes(labels)
-                labels = self._nested_gather(labels)
-                labels_host = labels if labels_host is None else nested_concat(labels_host, labels, padding_index=-100)
-            if inputs_decode is not None:
-                inputs_decode = self._pad_across_processes(inputs_decode)
-                inputs_decode = self._nested_gather(inputs_decode)
-                inputs_host = (
-                    inputs_decode
-                    if inputs_host is None
-                    else nested_concat(inputs_host, inputs_decode, padding_index=-100)
-                )
-            if logits is not None:
-                logits = self._pad_across_processes(logits)
-                logits = self._nested_gather(logits)
-                if self.preprocess_logits_for_metrics is not None:
-                    logits = self.preprocess_logits_for_metrics(logits, labels)
-                preds_host = logits if preds_host is None else nested_concat(preds_host, logits, padding_index=-100)
+            losses_host = self._pad_gather_concat(loss, losses_host, batch_size=batch_size, is_loss=True)
+            labels_host = self._pad_gather_concat(labels, labels_host)
+            inputs_host = self._pad_gather_concat(inputs_decode, inputs_host)
+            preds_host = self._pad_gather_concat(logits, preds_host, labels, None, is_logits=True)
 
             self.control = self.callback_handler.on_prediction_step(args, self.state, self.control)
 
             # Gather all tensors and put them back on the CPU if we have done enough accumulation steps.
             if args.eval_accumulation_steps is not None and (step + 1) % args.eval_accumulation_steps == 0:
-                if losses_host is not None:
-                    losses = nested_numpify(losses_host)
-                    all_losses = losses if all_losses is None else np.concatenate((all_losses, losses), axis=0)
-                if preds_host is not None:
-                    preds = nested_numpify(preds_host)
-                    all_preds = preds if all_preds is None else nested_concat(all_preds, preds, padding_index=-100)
-                if inputs_host is not None:
-                    inputs_decode = nested_numpify(inputs_host)
-                    all_inputs = (
-                        inputs_decode
-                        if all_inputs is None
-                        else nested_concat(all_inputs, inputs_decode, padding_index=-100)
-                    )
-                if labels_host is not None:
-                    labels = nested_numpify(labels_host)
-                    all_labels = (
-                        labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
-                    )
-
+                all_losses = self._numpify_concat_truncate(losses_host, all_losses, is_nested=False)
+                all_preds = self._numpify_concat_truncate(preds_host, all_preds)
+                all_inputs = self._numpify_concat_truncate(inputs_host, all_inputs)
+                all_labels = self._numpify_concat_truncate(labels_host, all_labels)
                 # Set back to None to begin a new accumulation
                 losses_host, preds_host, inputs_host, labels_host = None, None, None, None
 
         if args.past_index and hasattr(self, "_past"):
             # Clean the state at the end of the evaluation loop
             delattr(self, "_past")
-
-        # Gather all remaining tensors and put them back on the CPU
-        if losses_host is not None:
-            losses = nested_numpify(losses_host)
-            all_losses = losses if all_losses is None else np.concatenate((all_losses, losses), axis=0)
-        if preds_host is not None:
-            preds = nested_numpify(preds_host)
-            all_preds = preds if all_preds is None else nested_concat(all_preds, preds, padding_index=-100)
-        if inputs_host is not None:
-            inputs_decode = nested_numpify(inputs_host)
-            all_inputs = (
-                inputs_decode if all_inputs is None else nested_concat(all_inputs, inputs_decode, padding_index=-100)
-            )
-        if labels_host is not None:
-            labels = nested_numpify(labels_host)
-            all_labels = labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
-
-        # Number of samples
+    
+        # Gather all remaining tensors and put them back on the CPU, truncate by num_samples
         num_samples = len(eval_dataset)
-
-        if num_samples == 0 and observed_num_examples > 0:
-            num_samples = observed_num_examples
-
-        # Number of losses has been rounded to a multiple of batch_size and in a distributed training, the number of
-        # samplers has been rounded to a multiple of batch_size, so we truncate.
-        if all_losses is not None:
-            all_losses = all_losses[:num_samples]
-        if all_preds is not None:
-            all_preds = nested_truncate(all_preds, num_samples)
-        if all_labels is not None:
-            all_labels = nested_truncate(all_labels, num_samples)
-        if all_inputs is not None:
-            all_inputs = nested_truncate(all_inputs, num_samples)
+        all_losses = self._numpify_concat_truncate(losses_host, all_losses, num_samples=num_samples, is_nested=False)
+        all_preds = self._numpify_concat_truncate(preds_host, all_preds, num_samples=num_samples)
+        all_inputs = self._numpify_concat_truncate(inputs_host, all_inputs, num_samples=num_samples)
+        all_labels = self._numpify_concat_truncate(labels_host, all_labels, num_samples=num_samples)
 
         # Metrics!
+        metrics = {}
         if self.compute_metrics is not None and all_preds is not None and all_labels is not None:
             if args.include_inputs_for_metrics:
                 metrics = self.compute_metrics(
@@ -2309,12 +2268,9 @@ class Trainer:
                 )
             else:
                 metrics = self.compute_metrics(EvalPrediction(predictions=all_preds, label_ids=all_labels))
-        else:
-            metrics = {}
 
         # To be JSON-serializable, we need to remove numpy types or zero-d tensors
         metrics = denumpify_detensorize(metrics)
-
         if all_losses is not None:
             metrics[f"{metric_key_prefix}_loss"] = all_losses.mean().item()
         if hasattr(self, "jit_compilation_time"):
